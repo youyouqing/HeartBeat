9ed754941599b98fc40e4b8b6bad109f7e19177c
xiekang <xiekang@xiekangdeimac.local>
2017-03-23 16:29:05 +0800

    Initial Commit

---
+++ 摄像心率/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  摄像心率
+//
+//  Created by xiekang on 17/3/23.
+//  Copyright © 2017年 ZM. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
